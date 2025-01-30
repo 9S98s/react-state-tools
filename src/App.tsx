4c802a5912bc575d6b33949086542ac1bc01d611
윤효진 <hyojinee@dgsw.hs.kr>
2025-01-30 11:07:44 +0900

    feat :: 카운터

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-const App = () => {
+import { useCountStore } from "./store/count";
+
+export default function App() {
+  const count = useCountStore(state => state.count)
+  const increase = useCountStore(state => state.increase)
+  const decrease = useCountStore(state => state.decrease)
+
   return (
     <div>
-      <>앱 영역</>
+      <h2>{count}</h2>
+      <button onClick={increase}>+1</button>
+      <button onClick={decrease}>-1</button>
     </div>
   )
 }
-
-export default App
