c2ce887391dbb13b4f4788fff119a0f6ae2207c9
윤효진 <hyojinee@dgsw.hs.kr>
2025-01-30 11:18:46 +0900

    feat :: action 분리

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import { useCountStore } from "./store/count";
 
 export default function App() {
   const count = useCountStore(state => state.count)
-  const increase = useCountStore(state => state.increase)
-  const decrease = useCountStore(state => state.decrease)
+  const { increase, decrease } = useCountStore(state => state.actions)
 
   return (
-    <div>
+    <>
       <h2>{count}</h2>
-      <button onClick={increase}>+1</button>
-      <button onClick={decrease}>-1</button>
-    </div>
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
+    </>
   )
 }
--- src/store/count.ts
+++ src/store/count.ts
@@ -1,11 +1,15 @@
-import { create } from 'zustand'
+import { create } from 'zustand';
 
 export const useCountStore = create<{
-  count: number
-  increase: () => void
-  decrease: () => void
+    count: number;
+    actions: {
+        increase: () => void;
+        decrease: () => void;
+    }
 }>(set => ({
-  count: 1,
-  increase: () => set(state => ({ count: state.count + 1 })),
-  decrease: () => set(state => ({ count: state.count - 1 }))
+    count: 0,
+    actions: {
+        increase: () => set(state => ({ count: state.count + 1 })),
+        decrease: () => set(state => ({ count: state.count - 1 }))
+    }
 }))
